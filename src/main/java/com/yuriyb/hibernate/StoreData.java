8ccc39e5d33cac092369e848de6bedc7b3c49a72
YuriyB <yuriybpost@gmail.com>
2015-04-18 03:23:57 +0200

    added StoreData.java for running application and creation of tables in db

---
+++ src/main/java/com/yuriyb/hibernate/StoreData.java
@@ -0,0 +1,38 @@
+package com.yuriyb.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+public class StoreData {
+	public static void main(String[] args) {
+		
+		AnnotationConfiguration cfg = new AnnotationConfiguration();
+		
+		Session session = cfg.configure("hibernate.cfg.xml").buildSessionFactory().openSession();
+
+		Transaction t = session.beginTransaction();
+
+		Employee e1 = new Employee();
+		e1.setName("Alex Newton");
+
+		RegularEmployee e2 = new RegularEmployee();
+		e2.setName("Andrew Guk");
+		e2.setSalary(20000);
+		e2.setBonus(4);
+
+		ContractEmployee e3 = new ContractEmployee();
+		e3.setName("Artur Konandoyl");
+		e3.setPay_per_hour(1000);
+		e3.setContract_duration("12 hours");
+
+		session.persist(e1);
+		session.persist(e2);
+		session.persist(e3);
+
+		t.commit();
+		session.close();
+		
+		System.out.println("The data has been saved into DB!");
+	}
+}
